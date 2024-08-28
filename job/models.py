3a9a5da74bb9bd4003b74f418f3d1c4b6fbe2426
m7md158 <m7md.a7md158@gmail.com>
2024-08-28 19:24:06 +0300

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
+'''
+django models fields
+    - html widget
+    - validation
+    - db size
+'''
+
+JOB_TYPE = {
+    ("Full Time","Full Time"),
+    ("Part Time","Part Time")
+}
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    # location
+    job_type = models.CharField(max_length=15, choices=JOB_TYPE)
+    description = models.TextField()
+    published_at = models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+
+
+    def __str__(self):
+        return self.title
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # apps
+    'accounts', 'blog', 'contact',
+    'job', 'home'
 ]
 
 MIDDLEWARE = [
