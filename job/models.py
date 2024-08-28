6a6f9aa0b8c8ddbd6d294cafabf9a24fc8ce72dc
m7md158 <m7md.a7md158@gmail.com>
2024-08-28 20:44:47 +0300

    add job views & url

--- job/models.py
+++ job/models.py
@@ -22,7 +22,17 @@ class Job(models.Model):
     Vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
-
+    category = models.ForeignKey('Category',on_delete= models.CASCADE, blank= True) 
 
     def __str__(self):
         return self.title
+    
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    
